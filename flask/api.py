f34a6661de0566ce84385571dd11d56f207283e2
princerxj <pr273582@gmail.com>
2026-01-02 10:09:20 +0530

    Implementing full CRUD REST APi for todolist using Flask

---
+++ flask/api.py
@@ -0,0 +1,61 @@
+### Put and Delete 
+### Working with API's--JSON
+from flask import Flask, jsonify, request 
+
+app = Flask(__name__)
+
+### Initial Data in my to do list 
+items = [
+    {'id' : 1, 'name' : 'Item1', 'description' : 'This is my item1'},
+    {'id' : 2, 'name' : 'Item2', 'description' : 'This is my item2'}
+]
+
+@app.route('/')
+def home():
+    return 'Welcome to the sample Todo List App'
+
+## Get : retrieve all the items
+@app.route('/items', methods = ['GET'])
+def get_items():
+    return jsonify(items)
+
+## get : Retrive a specific item by id
+@app.route('/items/<int:item_id>', methods = ['GET'])
+def get_item(item_id):
+    item = next((item for item in items if item["id"]==item_id), None)
+    if item is None:
+        return jsonify({"error":"Item Not Found"})
+    return jsonify(item)
+
+## Post : Create a new task
+@app.route('/items', methods = ['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({"error" : "item not found"})
+    new_item = {
+        "id" : items[-1]["id"] + 1 if items else 1,
+        "name" : request.json['name'],
+        "description" : request.json['description']
+    }
+    items.append(new_item)
+    return jsonify(new_item)
+
+## Put : Update an existing item 
+@app.route('/items/<int:item_id>', methods = ['PUT'])
+def update_item(item_id):
+    item = next((item for item in items if item["id"]==item_id), None)
+    if item is None:
+        return jsonify({"error":"Item Not Found"})
+    item['name'] = request.json.get('name', item['name'])
+    item['description'] = request.json.get('description', item['description'])
+    return jsonify(item)
+
+## DELETE : Delete an item
+@app.route('/items/<int:item_id>', methods = ['DELETE'])
+def delete_item(item_id):
+    global items
+    items = [item for item in items if item["id"] != item_id]
+    return jsonify({"result" : "Item deleted"})
+
+if __name__ == "__main__" :
+    app.run(debug=True)
